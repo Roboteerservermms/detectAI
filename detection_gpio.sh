40b151916cf2e6076608b9bc8a908d6fae6d9097
ing03201 <35830057+ing03201@users.noreply.github.com>
2021-02-22 16:14:58 +0900

    Update detection_gpio.sh

--- detection_gpio.sh
+++ detection_gpio.sh
@@ -14,17 +14,6 @@ for gpio in "65" "74" "111" "113"; do
         echo "out" > /sys/class/gpio/gpio${gpio}/direction
 done
 
-audio_gpio="112"
-GPIO_DIR=$(ls /sys/class/gpio/ | grep ${audio_gpio})
-if [ -n "$GPIO_DIR" ]; then
-        echo "GPIO already exist from past error"
-        echo ${audio_gpio} > /sys/class/gpio/unexport
-        sleep 1
-fi
-echo ${audio_gpio} > /sys/class/gpio/export
-sleep 1
-echo "in" > /sys/class/gpio/gpio${audio_gpio}/direction
-
 export PYTHONPATH="/home/orangepi/detectAI"
 cd $PYTHONPATH
 g++ -o pir pir.cpp -std=c++11
