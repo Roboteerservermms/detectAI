0a418de9aefe4e84135883ae68356bb23910338c
ing03201 <35830057+ing03201@users.noreply.github.com>
2021-03-15 14:18:39 +0900

    Update LoRa.py

--- LoRa.py
+++ LoRa.py
@@ -90,7 +90,7 @@ def writeThread(ser, exitThread):
                 log.info("pir detect")
                 command = "PIR:LIGHTON"
                 os.system('echo 0 > /sys/class/gpio/gpio113/value')
-            os.system('echo 1 > /sys/class/gpio/gpio65/value & echo 1 > /sys/class/gpio/gpio74/value')
+            os.system('echo 1 > /sys/class/gpio/gpio65/value & echo 0 > /sys/class/gpio/gpio74/value')
             log.info("{0} command send to {1}".format(command, eui_data))
             ser.write(bytes(("AT+DATA={0}:{1}:\r\n").format(eui_data, command),'ascii'))
             ser.flush()
@@ -103,7 +103,7 @@ def writeThread(ser, exitThread):
             	if t >= ontime:
                     log.info("light off")
                     on_state = False
-                    os.system('echo 0 > /sys/class/gpio/gpio65/value & echo 0 > /sys/class/gpio/gpio74/value')
+                    os.system('echo 0 > /sys/class/gpio/gpio65/value & echo 1 > /sys/class/gpio/gpio74/value')
 
 if __name__ == "__main__":
     global ontime
