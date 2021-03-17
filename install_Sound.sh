a52e208d80df856e2f9ee44f2dbfd23775b1c2eb
kim <ing03201@gmail.com>
2021-03-17 10:27:42 +0900

    add installation_guide.sh

--- install_Sound.sh
+++ install_Sound.sh
@@ -18,3 +18,22 @@ sudo apt-get install portaudio19-dev python-pyaudio -y
 sudo python3.7 -m pip install pyaudio
 sudo python3.7 -m pip install matplotlib
 sudo python3.7 -m pip install pandas
+
+
+	sudo apt-get install llvm-10 -y
+	sudo ln -s /usr/bin/llvm-config-7 /usr/bin/llvm-config
+	sudo apt-get install build-essential python3.7 python3-scipy python-scipy cmake unzip pkg-config gcc-6 g++-6 screen libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libopenblas-dev libatlas-base-dev liblapack-dev gfortran libhdf5-serial-dev python3-dev python3-tk python-imaging-tk libgtk-3-dev -y
+	sudo apt-get install build-essential cmake unzip pkg-config gcc-6 g++-6 screen libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libopenblas-dev libatlas-base-dev liblapack-dev gfortran libhdf5-serial-dev python3.7-dev python3.7-tk python-imaging-tk libgtk-3-dev -y
+	sudo LLVM_CONFIG=/usr/bin/llvm-config python3.7 -m pip install llvmlite==0.29.0
+	sudo ln -s /usr/bin/llvm-config-10 /usr/bin/llvm-config
+	
+	sudo python3.7 -m pip install Cython 
+	sudo python3.7 -m pip install numpy
+	sudo python3.7 -m pip install librosa
+	sudo python3.7 -m pip install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_aarch64.whl
+
+	sudo apt-get install portaudio19-dev python-pyaudio -y
+	sudo python3.7 -m pip install pyaudio
+	sudo python3.7 -m pip install matplotlib
+	sudo python3.7 -m pip install pandas
+
