8d4ef3420f40e353d060ba0f51a0a48da8d15414
kim <kim>
2021-04-09 18:45:43 +0900

    fixing ctrl vlc

--- detection_gpio.sh
+++ detection_gpio.sh
@@ -4,15 +4,15 @@ chmod 775 /sys/class/gpio/export
 chmod 775 /sys/class/gpio/unexport
 echo "Start AI Detection!"
 for gpio in "65" "74" "111" "112" "113"; do
-        GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
-        if [ -n "$GPIO_DIR" ]; then
-                echo "GPIO already exist from past error"
-                echo ${gpio} > /sys/class/gpio/unexport
-                sleep 1
-        fi
-        echo ${gpio} > /sys/class/gpio/export
-        sleep 1
-        echo "out" > /sys/class/gpio/gpio${gpio}/direction
+    GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
+    if [ -n "$GPIO_DIR" ]; then
+            echo "GPIO already exist from past error"
+            echo ${gpio} > /sys/class/gpio/unexport
+            sleep 1
+    fi
+    echo ${gpio} > /sys/class/gpio/export
+    sleep 1
+    echo "out" > /sys/class/gpio/gpio${gpio}/direction
 done
 echo 1 > /sys/class/gpio/gpio74/value ## to control relay
 export PYTHONPATH="/home/orangepi/detectAI"
@@ -23,7 +23,12 @@ python3 LoRa.py &
 echo "LoRa start!" &
 python3 app.py &
 echo "server start!"&
-sudo -u orangepi -H sh -c "vlc --fullscreen --video-on-top ./playlist/video.mp4"
-vlc-ctrl pause
+sudo -u orangepi -H sh -c "vlc  --loop --fullscreen --video-on-top"
+video_list=$(ls -tr ./playlist/)
+for num in video_list; do
+    sudo -u orangepi -H sh -c "vlc --one-instance --playlist-enqueue ./playlist/$num"
+done
+sudo -u orangepi -H sh -c "vlc-ctrl play"
+sudo -u orangepi -H sh -c "vlc-ctrl pause"
 
 
