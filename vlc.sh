7289b4e8040f817339eefb7db6e6d7782fb14ff8
kim <kim>
2021-04-09 19:30:29 +0900

    add sleep time

--- vlc.sh
+++ vlc.sh
@@ -2,5 +2,6 @@ video_list=$(ls -tr ./playlist/)
 for num in $video_list; do
     sudo -u orangepi -H sh -c "vlc --one-instance --playlist-enqueue ./playlist/$num"
 done
+sleep 7
 sudo -u orangepi -H sh -c "vlc-ctrl play"
 sudo -u orangepi -H sh -c "vlc-ctrl pause"
