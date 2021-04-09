98ebdb09ff75ae963f32cea910ace3867e2dfd91
kim <kim>
2021-04-09 20:17:15 +0900

    fixing command

--- vlc.sh
+++ vlc.sh
@@ -2,4 +2,3 @@ video_list=$(ls -tr ./playlist/)
 for num in $video_list; do
     sudo -u orangepi -H sh -c "vlc  --loop --fullscreen --video-on-top --one-instance --playlist-enqueue ./playlist/$num"
 done
-sudo -u orangepi -H sh -c "vlc-ctrl pause"
