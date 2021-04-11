2b8e317c01004f57eb983cc01053c6f08196f35b
kim <kim>
2021-04-11 13:47:01 +0900

    simple func

--- vlc.py
+++ vlc.py
@@ -1,37 +1,27 @@
+# importing vlc module
+import vlc
 import os, subprocess
-import signal, threading
-import logging
-log = logging.getLogger('detect')
-log.setLevel(logging.DEBUG)
-log_handler = logging.StreamHandler()
-log.addHandler(log_handler)
+
+video_dir="./playlist"
+video_path="./playlist/"
+
+instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+player = instance.media_list_player_new()
+media_list = instance.media_list_new()
+video_list = os.listdir(video_dir)
+for v in video_list:
+    media = instance.media_new(v)
+    media_list.add_media(video_path + media)
+
+player.set_media_list(media_list)
+player.play()
 
 def str2bool(v):
    return str(v).lower() in ("yes", "true", "t", "1")
 
-def handler(signum, frame):
-    global exitThread
-    exitThread = True
-
-def play_and_pause():
-    os.system("bash vlc.sh")
+while True:
     object_detect = str2bool(subprocess.getoutput('cat /sys/class/gpio/gpio65/value'))
-    while exitThread:
-        if object_detect:
-            os.system("vlc-ctrl play")
-        else:
-            os.system("vlc-ctrl pause")
-
-
-
-if __name__ == "__main__":
-    exitThread = False
-    #종료 시그널 등록
-    signal.signal(signal.SIGINT, handler)
-    #시리얼 열기
-    #시리얼 읽을 쓰레드 생성
-    log.info("vlc play running!")
-    
-    vlc_player = threading.Thread(target=play_and_pause)
-    #시작!
-    vlc_player.start()
+    if object_detect:
+        player.play()
+    else:
+        player.pause()
