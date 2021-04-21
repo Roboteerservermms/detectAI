f38a881409ae7c5077da80312a4b0161094a5128
kim <kim>
2021-04-21 12:13:41 +0900

    fix installation

---
+++ installation/root_install.sh
@@ -0,0 +1,2 @@
+pip3 install urlopen schedule playsound 
+pip3 install youtube-dl pafy python-vlc pyserial xmltodict
--- installation/user_install.sh
+++ installation/user_install.sh
@@ -1,8 +1,6 @@
 sudo rm -rf /var/lib/dpkg/lock*
 sudo apt-get update
 sudo apt-get -y upgrade
-pip3 install urlopen schedule playsound 
 sudo apt-get install -y python3-json* python3-urllib3 python3-pandas 
-pip3 install youtube-dl pafy python-vlc pyserial vlc-ctrl 
 sudo apt install python3-gst-1.0 python3-dbus vlc-*
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
