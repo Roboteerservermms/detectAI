294d2298e2beb198726dce5b5d8759398fb02154
kim <kim>
2021-05-03 16:37:36 +0900

    fdsrasdf

--- installation/user_install.sh
+++ installation/user_install.sh
@@ -8,5 +8,10 @@ sudo apt-get install -y python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev \
     libtiff5-dev libx11-6 libx11-dev xfonts-base xfonts-100dpi xfonts-75dpi \
     xfonts-cyrillic fluid-soundfont-gm  \
     libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+    
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
 
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
